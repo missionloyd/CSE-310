fa5dea2548f8201b1a82966d7931b5b3d4dbf0ac
SirHoraceClock <79132357+SirHoraceClock@users.noreply.github.com>
2021-02-16 11:10:25 -0700

    Update insertionSort.cpp
    I included a function that solves for inversion. It was mentioned in lecture 4.

--- c++/tchow/insertionSort.cpp
+++ c++/tchow/insertionSort.cpp
@@ -5,9 +5,29 @@ using namespace std;
 //Prints out element wise comparisons
 
 /* Function to sort an array using insertion sort*/
+void inversionCounter(int arr[], int n){
+    int b, a;
+    int inversion=0;
+    for (int a = 0; a < n; a++)
+    {
+        b=a+1;
+        while(b<n){
+            if(arr[a]>arr[b]){
+                inversion++;
+                cout <<"<"<<a+1<<","<<b+1<<"> " ;
+            }
+            b++;
+        }
+    }
+    cout <<"\n" ;
+    cout<<"Total inversions = "<<inversion<<"\n";
+    cout<<"Running time of insertion sort is BigTheta("<<n<<" + "<<inversion<<").\n";
+}
 void insertionSort(int arr[], int n)
 {
+
     int j, key, i;
+    //int inversion=0;
     for (j = 1; j < n; j++)
     {
         key = arr[j];
@@ -21,6 +41,8 @@ void insertionSort(int arr[], int n)
         {
             cout<<arr[i]<<">"<<key<<"\n";
             arr[i + 1] = arr[i];
+            //inversion++;
+            //cout<<"Inversion = "<<inversion<<"\n"; //if you want to see when inversions are made
             i = i - 1;
 
         }
@@ -29,6 +51,8 @@ void insertionSort(int arr[], int n)
         }
         arr[i + 1] = key;
     }
+    //cout<<"Total inversions = "<<inversion<<"\n";
+    //cout<<"Running time of insertion sort is BigTheta("<<n<<" + "<<inversion<<").\n";
 }
 
 // A utility function to print an array of size n
@@ -43,11 +67,16 @@ void printArray(int arr[], int n)
 /* Driver code */
 int main()
 {
-    int arr[] = { 19,16,13,25,22};
+    //int arr[] = { 3,5,2,8,3}; //These are test inputs
+    //int arr[] = { 1,3,3,5,8}; //These are test inputs
+    int arr[] = { 5,4,3,2,1};
     int n = sizeof(arr) / sizeof(arr[0]);
 
+    inversionCounter(arr,n);
     insertionSort(arr, n);
     printArray(arr, n);
 
+
     return 0;
 }
+
